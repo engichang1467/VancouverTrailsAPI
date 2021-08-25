ddfa21f91ac7cc9762074bbef72d46b526ddb896
Michael Chang <mchang0926@gmail.com>
2021-08-25 15:28:59 -0700

    remove testTmp

--- testTmp.py
+++
@@ -1,9 +0,0 @@
-from dotenv import dotenv_values
-
-config = dotenv_values(".env")
-print(config)
-print(config['mainDB'])
-print(config['password'])
-print(config['DBName'])
-print(config['hostName'])
-print(config['portNum'])
