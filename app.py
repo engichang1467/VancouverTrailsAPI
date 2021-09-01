bbb638ba28b78a4e5bf846146f6725392f3a9c9e
Michael Chang <mchang0926@gmail.com>
2021-09-01 11:27:53 -0700

    allow the API to connect to the frontend

--- app.py
+++ app.py
@@ -6,7 +6,17 @@
 '''
 from fastapi import FastAPI
 from routes.trail import trail
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Connect to the frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='http://localhost:3000',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(trail)
